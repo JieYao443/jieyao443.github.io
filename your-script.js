9f5ba7f2eeb959a38782ee82ee3957aa1bf77023
JieYao443 <115999379+JieYao443@users.noreply.github.com>
2024-01-16 13:46:29 +0800

    upate all

---
+++ your-script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var audio = document.getElementById('backgroundMusic');
+  
+    // Play audio automatically when the DOM is fully loaded
+    audio.play();
+  
+    // You can add more functionality or event listeners if needed
+  });
+  
