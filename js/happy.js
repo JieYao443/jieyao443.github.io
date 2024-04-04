09ac77fea010cbd8ff98472e64f424892fe2dff7
JieYao443 <115999379+JieYao443@users.noreply.github.com>
2024-04-04 22:09:40 +0800

    rev03

--- js/happy.js
+++ js/happy.js
@@ -9,22 +9,22 @@ $(function () {
     setTimeout(function () {
         $('.happy').animate({
             opacity:"1",
-            top:"15%"
+            top:"10%"
         },2000);
 
     },6000);
     setTimeout(function () {
+        $('.button-style0').animate({
+            opacity:"1",
+            top:"60%"
+        },1000);
         $('.button-style1').animate({
             opacity:"1",
             top:"70%"
         },1500);
         $('.button-style2').animate({
             opacity:"1",
-            top:"85%"
+            top:"80%"
         },2000);
     },9000);
-	 $(window).click(function(){
-	 	$('audio')[0].play();
-	 });
-	 window.audio=$('audio')[0].play();
 })
